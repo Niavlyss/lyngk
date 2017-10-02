a6af5dff023f5a54d7476d9ff36ad01bc6ff6f98
sehinger <sylvain.ehinger62@gmail.com>
2017-10-02 11:19:07 +0200

    #Commit 2 : réponse histoire 1.

--- src/Coordinates.js
+++ src/Coordinates.js
@@ -1,4 +1,14 @@
 "use strict";
 
 Lyngk.Coordinates = function (c, l) {
+    var colones=c;
+    var lignes=l;
+
+    //var estvalable="A3","B2-5","C1-7","D2-7","E2-8","F3-8","G3-9","H4-7","I7"
+
+    this.valable=function(){
+        if(colones=="A" && lignes==1){
+            return false;
+        }
+    }
 };
