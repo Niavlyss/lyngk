78c4817d59c34256537629f91d158aff4811eca5
sehinger <sylvain.ehinger62@gmail.com>
2017-10-02 12:00:40 +0200

    #Commit 3 : ajout debut réponse histoire 2 .

--- src/Coordinates.js
+++ src/Coordinates.js
@@ -4,11 +4,18 @@ Lyngk.Coordinates = function (c, l) {
     var colones=c;
     var lignes=l;
 
-    //var estvalable="A3","B2-5","C1-7","D2-7","E2-8","F3-8","G3-9","H4-7","I7"
+     // coord valable="A3","B2-5","C1-7","D2-7","E2-8","F3-8","G3-9","H4-7","I7"
+        var A = {l: "A", min: 3, max: 3};
+        var B = {l: "B", min: 2, max: 5};
+        var C = {l: "C", min: 1, max: 7};
+        var D = {l: "D", min: 2, max: 7};
+        var E = {l: "E", min: 2, max: 8};
+        var F = {l: "F", min: 3, max: 8};
+        var G = {l: "G", min: 3, max: 9};
+        var H = {l: "H", min: 4, max: 7};
+        var I = {l: "I", min: 7, max: 7};
 
     this.valable=function(){
-        if(colones=="A" && lignes==1){
-            return false;
-        }
+
     }
 };
