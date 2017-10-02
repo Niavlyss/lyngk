e7823e557cab519b04070d30fcce10f8de55b5c0
sehinger <sylvain.ehinger62@gmail.com>
2017-10-02 10:55:05 +0200

    Premier Commit, ajout test case valable.

--- test/test.js
+++ test/test.js
@@ -1,3 +1,8 @@
 'use strict';
 
 var LyngkTestCase = TestCase("LyngkTestCase");
+
+LyngkTestCase.prototype.test1=function() {
+    var coord = new Lyngk.Coordinates("A", 1);
+    assertTrue(coord.valable()==false);-
+}
