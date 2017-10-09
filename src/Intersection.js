a67e362dd0a864b04ae7446e36fecbaf08bb34b7
sylvain <sylvain.ehinger62@gmail.com>
2017-10-09 11:44:18 +0200

    Commit #9 : Ajout réponse histoire 8

--- src/Intersection.js
+++ src/Intersection.js
@@ -2,10 +2,22 @@
 
 Lyngk.State = {VACANT: 0, ONE_PIECE: 1, STACK: 2, FULL_STACK: 3};
 
-Lyngk.Intersection = function () {
+Lyngk.Intersection = function (c) {
+    this.c=new Lyngk.Coordinates();
     var etat=Lyngk.State.VACANT;
+    var color=Lyngk.Color.BLUE;
 
     this.getState=function () {
         return etat;
+    }
+
+    this.getColor=function(){
+        return color;
+    }
+
+
+    this.placer=function(pionColor){
+        etat=Lyngk.State.ONE_PIECE;
+        color=pionColor;
     }
 };
